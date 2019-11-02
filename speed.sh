325899b9fa570ae02c120ece6a66b565a2587f36
Ramón Salado <ramonsalado@gmail.com>
2019-11-02 11:02:04 +0100

    Update speed.sh

--- speed.sh
+++ speed.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Script almacenar un histórico de velocidades de red
 #Dependencia: Speedtest-cli
+#Posibilidad de implementar Gnuplot para generación de gráficos
 
 #echo "IP,Date,Day,Time,Hour,Download,Upload" > speeds.csv
 
-#IP address 
+#Dirección IP 
 dig +short myip.opendns.com @resolver1.opendns.com  | tr '\n' ',' >>speeds.csv
 
-#date string
+#Fecha
 date +%Y-%m-%d  | tr '\n' ',' >>speeds.csv
 date +%u  | tr '\n' ',' >>speeds.csv
 
-#time
+#Hora
 date +"%T" | tr '\n' ',' >>speeds.csv
-#then add hour, so can look at averages per hour of the day
 date +"%H" | tr '\n' ',' >>speeds.csv
 
 #speedtest-cli --simple | tr '\n' ';' >>speeds.csv
@@ -21,13 +21,14 @@ date +"%H" | tr '\n' ',' >>speeds.csv
 results=$(speedtest-cli --simple)
 concat=$(speedtest-cli --simple | tr '\n' ',')
 
-#download speed and add to file
+#Velocidad de Descarga
 echo $concat | awk '{printf $4}'>>speeds.csv
 
-#add comma 
+#Separador
 printf "," >>speeds.csv
 
-#Upload speed
+#Velocidad de Carga
 echo $concat | awk '{printf $6}'>>speeds.csv
-#next print
+
+#Next
 printf '\n' >>speeds.csv
